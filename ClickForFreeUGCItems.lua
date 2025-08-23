7b8d1a3a434a9a16d7fdb5124253e29fd4e81e62
Games1799 <waserwill2000@gmail.com>
2025-08-24 00:04:39 +0300

    ClickForFreeUGCItems.lua

--- ClickForFreeUGCItems.lua
+++ ClickForFreeUGCItems.lua
@@ -1,3 +1,6 @@
+local StarterGui = game:GetService("StarterGui")
+local VirtualUser = game:GetService("VirtualUser")
+
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
 local Window = Library:NewWindow("Click For Free UGC Items")
@@ -21,4 +24,15 @@ Section:CreateToggle("Автокликер", function(state)
             task.wait()
         end
     end)
+end)
+
+Section:CreateButton("Анти афк", function()
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    VirtualUser:CaptureController()VirtualUser:ClickButton2(Vector2.new())
+end)
+StarterGui:SetCore("SendNotification", {
+    Title = "Успешно!",
+    Text = "Анти афк активирован",
+    Duration = 5
+})
 end)
