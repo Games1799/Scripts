b60840ed537c32c2f7b0af926a9f604a354f2e73
Games1799 <waserwill2000@gmail.com>
2025-08-24 00:56:07 +0300

    ClickForFreeUGCItems.lua

--- ClickForFreeUGCItems.lua
+++ ClickForFreeUGCItems.lua
@@ -1,6 +1,7 @@
+local PlaceId = game.PlaceId
 local StarterGui = game:GetService("StarterGui")
 local VirtualUser = game:GetService("VirtualUser")
-
+if PlaceId == 99096766260885 then
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
 local Window = Library:NewWindow("Click For Free UGC Items")
@@ -67,3 +68,10 @@ end)
 Section:CreateButton("Скрыть это окно", function()
 game:GetService("CoreGui").WizardLibrary.Container["\208\161\208\190\208\183\208\180\208\176\209\130\208\181\208\187\209\140\209\129\208\186\209\128\208\184\208\191\209\130\208\176Window"]:Destroy()
 end)
+else 
+StarterGui:SetCore("SendNotification", {
+        Title = "Ошибка!",
+        Text = "Этот плейс не подходит для скрипта",
+        Duration = 5
+    })
+end
