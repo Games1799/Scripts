a15b777633e8628eb19351ee75f8d652c7de42a2
Games1799 <waserwill2000@gmail.com>
2025-11-03 02:38:25 +0300

    FlexUgcCodesSnaiper.lua

---
+++ FlexUgcCodesSnaiper.lua
@@ -0,0 +1,20 @@
+local code = getgenv().code
+local time = getgenv().time
+local TestMode = getgenv().TestMode
+local RunService = game:GetService("RunService")
+local Conn
+if getgenv().TestMode then
+conn = RunService.RenderStepped:Connect(function()
+    if tick() >= time - 0.5 then
+        game:GetService("ReplicatedStorage").RedeemCode:InvokeServer(code)
+        conn:Disconnect()
+    end
+end)
+else
+conn = RunService.RenderStepped:Connect(function()
+    if tick() >= time then
+        game:GetService("ReplicatedStorage").RedeemCode:InvokeServer(code)
+        conn:Disconnect()
+    end
+end)
+end
