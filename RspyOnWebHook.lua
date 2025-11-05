006d61e8d49952e6b3bb2af39f1b10f0c4b2e188
Games1799 <waserwill2000@gmail.com>
2025-11-06 00:21:15 +0300

    RspyOnWebHook.lua

---
+++ RspyOnWebHook.lua
@@ -0,0 +1,34 @@
+local httprequest = request or http_request or (syn and syn.request) or (http and http.request) or (fluxus and fluxus.request)
+local HttpService = game:GetService("HttpService")
+local StarterGui = game:GetService("StarterGui")
+
+local Webhook = getgenv().Webhook 
+if not Webhook then
+StarterGui:SetCore("SendNotification",{
+Title = "Snaiper error",
+Text = "Ошибка — введите вебхук",
+Duration = 3,
+Button1 = "Ок"
+})
+return
+end
+
+local mt = getrawmetatable(game)
+setreadonly(mt,false)
+local old = mt.__namecall
+mt.__namecall = newcclosure(function(self,...)
+local method = getnamecallmethod()
+local args = {...}
+local class = self.ClassName
+if (class == "RemoteEvent" and method == "FireServer") or
+(class == "RemoteFunction" and method == "InvokeServer") or
+(class == "BindableEvent" and method == "Fire") or
+(class == "BindableFunction" and method == "Invoke") or
+(class == "UnstableRemoteEvent" and method == "FireServer") then 
+local payload = HttpService:JSONEncode({content = "Событие: "..class.."\nМетод: "..method.."\nАргументы: "..HttpService:JSONEncode(args)})
+pcall(function()
+httprequest({Url = Webhook,Method = "POST",Headers = { ["Content-Type"] = "application/json" },Body = payload})
+end)
+end
+return old(self,...)
+end)
