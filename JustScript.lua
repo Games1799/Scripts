0eb3c4cd9fa1a78ecb9f5888b70bdcf2f6088329
Games1799 <waserwill2000@gmail.com>
2025-12-31 17:11:09 +0300

    JustScript.lua

--- JustScript.lua
+++ JustScript.lua
@@ -50,7 +50,7 @@ end)
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local Window = Library:NewWindow("Just script v2.1")
+local Window = Library:NewWindow("Just script v2.1 ☃️")
 
 local Section = Window:NewSection("Полезные скрипты")
 
