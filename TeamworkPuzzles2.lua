608d1ade071a7407bfc1bd4036b0424972215f56
Games1799 <waserwill2000@gmail.com>
2026-01-07 19:58:22 +0300

    Add TeamworkPuzzles2.lua for milk collection feature

---
+++ TeamworkPuzzles2.lua
@@ -0,0 +1,18 @@
+local workspace = game:GetService("Workspace")
+local char = game.Players.LocalPlayer.Character
+if not char then return nil end
+
+local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local Window = Library:NewWindow("Teamwork Puzzles 2")
+local Section = Window:NewSection("Основное")
+
+Section:CreateButton("Собрать молоко", function()
+for _, v in ipairs(workspace.MilkCollection:GetDescendants()) do
+if v:IsA("TouchTransmitter") then
+char:PivotTo(v.Parent:GetPivot())
+firetouchinterest(char.HumanoidRootPart,v.Parent,0)
+firetouchinterest(char.HumanoidRootPart,v.Parent,1)
+task.wait(1)
+end
+end
+end)
