a60871066a8f0d1fc7ea6bf1be0162625e9a3aca
Games1799 <waserwill2000@gmail.com>
2026-05-26 10:47:48 +0300

    Create TpTool.lua

---
+++ TpTool.lua
@@ -0,0 +1,16 @@
+local TpTool = Instance.new("Tool")
+TpTool.RequiresHandle = false
+TpTool.TextureId = "rbxassetid://6723742952"
+TpTool.Parent = game.Players.LocalPlayer:FindFirstChildOfClass("Backpack")
+
+local mouse = game.Players.LocalPlayer:GetMouse()
+TpTool.Activated:Connect(function()
+local root = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+if not root then 
+  return 
+end
+if not mouse.Target then 
+  return 
+end
+root.CFrame = CFrame.new(mouse.Hit.Position + Vector3.new(0,3,0))
+    
