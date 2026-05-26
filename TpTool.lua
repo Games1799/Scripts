43273a31a6013f203c67cd2c72583c6d064fe5fd
Games1799 <waserwill2000@gmail.com>
2026-05-26 10:49:03 +0300

    Update TpTool.lua

--- TpTool.lua
+++ TpTool.lua
@@ -5,12 +5,12 @@ TpTool.Parent = game.Players.LocalPlayer:FindFirstChildOfClass("Backpack")
 
 local mouse = game.Players.LocalPlayer:GetMouse()
 TpTool.Activated:Connect(function()
-local root = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-if not root then 
-  return 
-end
-if not mouse.Target then 
-  return 
-end
-root.CFrame = CFrame.new(mouse.Hit.Position + Vector3.new(0,3,0))
-    
+  local root = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+  if not root then 
+    return 
+  end
+  if not mouse.Target then 
+    return 
+  end
+  root.CFrame = CFrame.new(mouse.Hit.Position + Vector3.new(0,3,0))
+end)
