721d60461c71b7f3f40e144f5d0b2751955bac77
Games1799 <waserwill2000@gmail.com>
2026-06-23 01:41:52 +0300

    Update AntiENV.lua

--- AntiENV.lua
+++ AntiENV.lua
@@ -1,5 +1,6 @@
 if not isfile("ANTI_ENV_LOGGER") then
    writefile("ANTI_ENV_LOGGER", "true")
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Games1799/Scripts/refs/heads/main/AntiENV.lua"))()
 else
    print("Hello")
 end
