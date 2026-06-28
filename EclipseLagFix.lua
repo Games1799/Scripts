9c2bad783f9644ffd49ba5db4c85d180254a223f
Games1799 <waserwill2000@gmail.com>
2026-06-28 09:32:19 +0300

    Update EclipseLagFix.lua

--- EclipseLagFix.lua
+++ EclipseLagFix.lua
@@ -4,29 +4,12 @@ getgenv().new_c_closure = function(b) return b end
 getgenv().hookmetamethod = function(table, method, func)
    local mt = getrawmetatable(table)
    setreadonly(mt, false)
-   if method == "__namecall" then
-      local old = mt.__namecall
-      mt.__namecall = function(self, ...)
-         return func(self, ...)
-      end
-      return old
-   elseif method == "__index" then
-      local old = mt.__index
-      mt.__index = function(self, key, ...)
-         return func(self, key, ...)
-      end
-      return old
-   elseif method == "__newindex" then
-      local old = mt.__newindex
-      mt.__newindex = function(self, key, value, ...)
-         return func(self, key, value, ...)
-      end
-      return old
-   elseif method == "__tostring" then
-      local old = mt.__tostring 
-      mt.__tostring = function(self, ...)
-         return func(self, ...)
-      end
-      return old
+   local old = mt[method]
+   mt[method] = function(...)
+       return func(...)
    end
+   return old
 end
+
+getgenv().hook_meta_method = hookmetamethod
+getgenv().HookMetaMethod = hookmetamethod
